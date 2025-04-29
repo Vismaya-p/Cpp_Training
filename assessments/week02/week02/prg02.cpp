f0d7e35179c5172b390db31265cd37bca630db53
Vismaya-p <vismayaprabhakar1@gmail.com>
2025-04-29 13:02:11 +0530

    updated

--- assessments/week02/week02/prg02.cpp
+++ assessments/week02/week02/prg02.cpp
@@ -4,6 +4,71 @@ using namespace std;
 int main()
 {
 	int n;
+	int num0 = 0;
+	int num1 = 0;
+	int num2 = 0;
+	int num3 = 0;
+	int num4 = 0;
+	int num5 = 0;
+	int num6 = 0;
+	int num7 = 0;
+	int num8 = 0;
+	int num9 = 0;
+	cout << "enter a num: ";
+	cin >> n;
+	while (n != 0)
+	{
+		int i = 10;
+		int r = n % 10;
+		switch (r)
+		{
+		case 0:
+			num0++;
+			break;
+		case 1:
+			num1++;
+			break;
+		case 2:
+			num2++;
+			break;
+		case 3:
+			num3++;
+			break;
+		case 4:
+			num4++;
+			break;
+		case 5:
+			num5++;
+			break;
+		case 6:
+			num6++;
+			break;
+		case 7:
+			num7++;
+			break;
+		case 8:
+			num8++;
+			break;
+		case 9:
+			num9++;
+			break;
+		}
+		n /= 10;
+	}
+	cout << "digit frequencies:" << endl;
+	cout << "0:" << num0 << endl;
+	cout << "1:" << num1 << endl;
+	cout << "2:" << num2 << endl;
+	cout << "3:" << num3 << endl;
+	cout << "4:" << num4 << endl;
+	cout << "5:" << num5 << endl;
+	cout << "6:" << num6 << endl;
+	cout << "7:" << num7 << endl;
+	cout << "8:" << num8 << endl;
+	cout << "9:" << num9 << endl;
+
+}
+	/*{int n;
 	cout << "enter a num: ";
 	cin >> n;
 	for (int i = 0;i <= 9;i++)
@@ -20,5 +85,5 @@ int main()
 		
 		cout << i << ":" << count << endl;
 	}
-	return 0;
-	}
+	return 0;}*/
+	
--- assessments/week02/week02/prg03.cpp
+++ assessments/week02/week02/prg03.cpp
@@ -3,17 +3,22 @@
 using namespace std;
 int main()
 {
-	int digit, n, num, res = 0;
+	int digit=0, n, num, sum=0;
 	cout << "enter the n";
 	cin >> n;
 	num = n;
-	while (num != 0) {
-		digit = num % 10;
-		res += digit * digit * digit;
+	while (num > 0) {
+		digit++;
 		num /= 10;
-
 	}
-	if (res == n)
+	num = n;
+	while (num > 0)
+	{
+		sum = sum + pow(num % 10, digit);
+		num /= 10;
+	}
+
+	if (sum == n)
 		cout << "Armstrong num" << endl;
 	else
 		cout << "not an armstrong num" << endl;
