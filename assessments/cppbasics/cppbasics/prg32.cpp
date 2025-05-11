60615a8220f185042c3e444dc949318fff487a7f
Vismaya-p <vismayaprabhakar1@gmail.com>
2025-05-11 23:25:49 +0530

    updated

--- assessments/cppbasics/cppbasics/prg32.cpp
+++ assessments/cppbasics/cppbasics/prg32.cpp
@@ -4,10 +4,10 @@ using namespace std;
 int main() {
 	int n;
 	cin >> n;
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j < i; j++)
+	for (int i = n; i >= 1; i--) {
+		for (int j = 1; j <=n-i; j++)
 			cout << " ";
-		for (int j = 1; j <= 2 * (n - i) + 1; j++)
+		for (int j = 1; j <= (2 * i) - 1; j++)
 			cout << "*";
 		cout << endl;
 	}
--- assessments/cppbasics/cppbasics/prg61.cpp
+++ assessments/cppbasics/cppbasics/prg61.cpp
@@ -6,13 +6,17 @@ int main()
 	int freq[26] = { 0, };
 	char str[100];
 	cin.getline(str, 100);
-	for (int i = 0;i < 26;i++)
-	{
-		freq[i] = 0;
-	}
+
 	for (int i = 0;i < strlen(str);i++)
 	{
-		freq[str[i] - 'a']++;
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			freq[str[i] - 'a']++;
+		}
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			freq[str[i] - 'A']++;
+		}
 	}
 	for (int i = 0;i < 26;i++)
 	{
