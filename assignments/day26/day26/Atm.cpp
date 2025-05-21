f9c37acfbbad9eb5f6f1a1043fba01afb0196a6a
Vismaya-p <vismayaprabhakar1@gmail.com>
2025-05-21 17:53:09 +0530

    updated

--- assignments/day26/day26/Atm.cpp
+++ assignments/day26/day26/Atm.cpp
@@ -5,4 +5,72 @@ class atmAccount
 	float balance;
 	string AccHolder;
 
+public:
+	void withdraw();
+	void deposit();
+	void checkBalance();
+	void setBalance();
 };
+void atmAccount:: setBalance()
+{
+	balance = 35000;
+}
+void atmAccount::checkBalance()
+{
+	cout << "account balance: ";
+	cout << balance;
+}
+void atmAccount::deposit()
+{
+	int amount;
+	cout << "Enter the amount you want to deposit: " << endl;
+	cin >> amount;
+	balance = balance + amount;
+	cout << "amount deposited successfully" << endl;
+
+}
+void atmAccount:: withdraw()
+{
+	int amount;
+	cout << "Enter the amount you want to withdraw: "<<endl;
+	cin >> amount;
+	balance = balance - amount;
+	if (amount <= balance)
+	{
+		cout << "amount deducted successfully..." << endl;
+	}
+	else
+	{
+		cout << "Insufficient Balance";
+	}
+
+}
+int main()
+{
+	atmAccount a;
+	a.setBalance();
+	int choice = 0;
+	cout << "enter the choice(1.check balance,2.deposit,3.withdraw,4.Exit):" << endl;
+	do{
+	cin >> choice;
+		switch (choice)
+		{
+		case 1:
+			a.checkBalance();
+			break;
+		case 2:
+			a.deposit();
+			break;
+		case 3:
+			a.withdraw();
+			break;
+		case 4:
+			cout << "Exit" << endl;
+			break;
+		default:
+			cout << "Invalid choice";
+		}
+		
+	} while (choice != 4);
+	return 0;
+}
