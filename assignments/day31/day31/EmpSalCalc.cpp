904efce20ce9fcb813ae7aaa416c96af726a040a
Vismaya-p <vismayaprabhakar1@gmail.com>
2025-05-28 08:09:54 +0530

    updated

--- assignments/day31/day31/EmpSalCalc.cpp
+++ assignments/day31/day31/EmpSalCalc.cpp
@@ -6,29 +6,66 @@ class Employee
 	int empId;
 	string name;
 public:
-	Employee(int id,string n):empId(id),name(n){}
+	Employee(int empId,string name)
+	{
+		this->empId = empId;
+		this->name = name;
+	}
 };
 class Developer :public Employee
 {
 protected:
 	int coding_hours;
 public:
-	Developer(int id, string n,int h) :Employee(id, n)
+	Developer(int empId, string name,int coding_hours) :Employee(empId, name)
 	{
-		coding_hours = h;
+		this->coding_hours=coding_hours;
+	}
+	int calculateSalary()
+	{
+		return coding_hours * 500;
 	}
 };
 class Manager :public Employee
 {
+protected:
 	int team_size;
 public:
-	Manager(int id, string n,int s) :Employee(id, n)
+	Manager(int empId, string name,int team_size) :Employee(empId, name)
 	{
-		team_size = s;
+		this->team_size = team_size;
+	}
+	int calculateTeamBenefit()
+	{
+		return team_size*5000;
 	}
 };
 class TechLead :public Developer, public Manager
 {
 public:
-	TechLead(int id, string n, int s,int h):Employee(id,n),Developer(id,n,h),Manager(id,n,s){}
+	TechLead(int empId, string name, int team_size, int coding_hours):Developer(empId, name, coding_hours),Manager(empId, name, team_size) {}
+	int calculateSalary()
+	{
+		return coding_hours * 500 + team_size * 5000;
+	}
+	void display()
+	{
+		cout << "Tech Lead Info:" << endl;
+
+	}
+
 };
+int main()
+{
+	int empId, team_size, coding_hours;
+	string name;
+	cout << "Emp ID:";
+	cin >> empId;
+	cout << "Name: ";
+	cin >> name;
+	cout << "Coding Hours: ";
+	cin >> coding_hours;
+	cout << "Team Size: ";
+	cin >> team_size;
+
+}
