f16c7979b014ec7b38069d4f17b48733243c6869
Vismaya-p <vismayaprabhakar1@gmail.com>
2025-06-03 08:06:20 +0530

    updated

--- classwork/day35/day35/netMon.cpp
+++ classwork/day35/day35/netMon.cpp
@@ -1,33 +1,83 @@
 #include<iostream>
 #include<vector>
 #include<fstream>
-#include<string>
-#include<algorithm>
 
 using namespace std;
-enum logLevel
+
+enum LoggingLevel
 {
 	INFO,
 	DEBUG,
 	WARNING,
 	ERROR,
 };
-class logger { 
-	ofstream logFile; 
+
+class Packet
+{
+protected:
+	int packetId;
+	string protocol;
+	int size;
 public:
-	logger(const char* file)
+	void setId(int packetId)
 	{
-		logFile.open(file, ios::app);
-		if (!logFile.is_open())
+		this->packetId = packetId;
+	}
+	int getId()
+	{
+		return packetId;
+	}
+	void setProtocol(string protocol)
+	{
+		this->protocol = protocol;
+	}
+	string getProtocol()
+	{
+		return protocol;
+	}
+	void setSize(int size)
+	{
+		this->size = size;
+	}
+	int getSize()
+	{
+		return size;
+	}
+	void display()
+	{
+		cout << "ID: " << packetId << endl;
+		cout << "Protocol: " << protocol << endl;
+		cout << "Size: " << size << endl;
+	}
+};
+
+class Logger
+{
+	ofstream outFile;
+public:
+	Logger(const char* f1)
+	{
+		try
 		{
-			cout << "Error opening log file." << endl;
+			outFile.open(f1, ios::out);
+			if (!outFile.is_open())
+			{
+				throw "File already opened";
+			}
+		}
+		catch (string s)
+		{
+			cout << s << endl;
 			exit(0);
 		}
 	}
-	const char* getLevelName(int level)
+
+	const char* logleveltostring(int level)
 	{
-		switch(level)
-		{ 
+		switch (level)
+		{
+		default:
+			break;
 		case 0:
 			return "INFO";
 			break;
@@ -40,26 +90,133 @@ public:
 		case 3:
 			return "ERROR";
 			break;
-		default:
-			break;
 		}
 	}
-	void log(int level,const char* msg)
+
+	void log(int level, const char* str)
 	{
-		logFile << "[" << getLevelName(level) << "]" << msg << endl;
+		outFile << "[" << logleveltostring(level) << "]" << str << endl;
+		outFile.flush();
 	}
 };
 
-class Packet
+class Queue
 {
-	int packetId;
-	char protocol[10];
-	int size;
+private:
+	vector<Packet>arr;
 public:
-	void set(int id, const char* proto, int sz)
+	void loadPacket()
 	{
-		packetId = id;
-		strcpy(protocol, proto);
-		size = sz;
+		int pi, s;
+		string pr;
+		Packet p;
+		ifstream inFile;
+		try
+		{
+			inFile.open("packets.txt", ios::in);
+			if (!inFile.is_open())
+				throw"File cannot be opened";
+		}
+		catch (const char* s)
+		{
+			cout << s;
+			exit(0);
+		}
+		while (!inFile.eof())
+		{
+			inFile >> pi;
+			p.setId(pi);
+			inFile >> pr;
+			p.setProtocol(pr);
+			inFile >> s;
+			p.setSize(s);
+			arr.push_back(p);
+		}
+	}
+
+	void exportLog(Logger& lOb)
+	{
+
+		for (auto i : arr)
+		{
+			char msg[60];
+			sprintf(msg, "Packet ID: %d | Protocol: %s | Size %d", i.getId(), (i.getProtocol()).c_str(), i.getSize());
+			if (i.getSize() > 1000)
+			{
+				lOb.log(2, msg);
+			}
+			else
+			{
+				lOb.log(1, msg);
+			}
+			cout << endl;
+		}
+	}
+
+	void filterByProtocol(string proto)
+	{
+		for (auto i : arr)
+		{
+			if (i.getProtocol() == proto)
+			{
+				cout << "Packet" << i.getId() << " | " << i.getProtocol() << " | " << i.getSize() << endl;
+			}
+		}
+	}
+
+	void detectLargePackets()
+	{
+		for (auto i : arr)
+		{
+			if (i.getSize() > 1000)
+			{
+				cout << "Packet " << i.getId() << " | " << i.getProtocol() << " | " << i.getSize() << endl;
+			}
+		}
 	}
 };
+
+int main()
+{
+	Queue q;
+	Logger l("netmon_log.txt");
+	cout << "==== NetMon Packet Monitor ====" << endl;
+	cout << "1. Load Packet Data " << endl;
+	cout << "2. Filter by protocol" << endl;
+	cout << "3. Check for Large Packets " << endl;
+	cout << "4. Export Logs " << endl;
+	cout << "5. Exit" << endl;
+
+	while (1)
+	{
+		int options;
+		cin >> options;
+		switch (options)
+		{
+		default:
+			break;
+		case 1:
+			q.loadPacket();
+			break;
+		case 2:
+		{
+			string pro;
+			cin >> pro;
+			q.filterByProtocol(pro);
+			break;
+		}
+		case 3:
+			q.detectLargePackets();
+			break;
+		case 4:
+			cout << "In here\n";
+			q.exportLog(l);
+			break;
+		case 5:
+			exit(0);
+			break;
+		}
+	}
+
+
+}
