2b2dbf9d76e8d9bc776238eea27f1e6f8c6e4eb0
Vismaya-p <vismayaprabhakar1@gmail.com>
2025-06-04 20:13:38 +0530

    updated

--- assignments/day38/day38/taskManager.cpp
+++ assignments/day38/day38/taskManager.cpp
@@ -5,36 +5,43 @@ using namespace std;
 int main()
 {
 	list<string>tasks;
-	string task;
-	cout << "\nEnter Command(add/urgent/remove/show/exit): ";
-	getline(cin, task);
+	string input;
+	cout << "Enter Command(add/urgent/critical/remove/show/exit): ";
+	
 	while (true)
 	{
-		if (task == "exit")
+		getline(cin, input);
+		string command = input.substr(0, input.find(" "));
+		string task = input.substr(input.find(" ") + 1);
+		if (command == "exit")
 		{
 			break;
 		}
-		else if (task.find("critical") || (task.find("urgent")))
+		else if (command.find("critical") || (command.find("urgent")))
 		{
 			tasks.push_front(task);
 		}
-		else if (task.find("add"))
+		else if (command.find("add"))
 		{
 			tasks.push_back(task);
 		}
-		else if (task.find("remove"))
+		else if (command.find("remove"))
 		{
 			tasks.remove(task);
+		}
+		else if(command=="show")
+		{
+			cout << "List after insertions\n";
+			for (auto i : tasks)
+			{
+				cout << i << " ";
+			}
+			cout << endl;
 		}
 		else
 		{
 			cout << "Invalid output" << endl;
 		}
-		cout << "List after insertions\n";
-		for (auto i : tasks)
-		{
-			cout << task << " ";
-		}
-		return 0;
 	}
+	return 0;
 }
