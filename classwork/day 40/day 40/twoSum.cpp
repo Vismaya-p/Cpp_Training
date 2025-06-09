765c28b33b2d5ee39f433fd629ce6538c98d9fc3
Vismaya-p <vismayaprabhakar1@gmail.com>
2025-06-09 09:34:27 +0530

    updated

--- classwork/day 40/day 40/twoSum.cpp
+++ classwork/day 40/day 40/twoSum.cpp
@@ -5,24 +5,27 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-
+        vector<int> res;
         for (int i = 0; i < nums.size(); ++i) {
             for (int j = i + 1;j < nums.size();j++)
             {
                 if (nums[i] + nums[j] == target) {
-                    return { i,j };
+                    res.push_back(i);
+                    res.push_back(j);
+                    return res;
                 }
             }
         }
-        return {};
+        return res;
     }
 
 };
 int main()
 {
+    Solution s;
     vector<int>nums = { 2,7,11,15 };
     int target = 9;
-    vector<int>result = twoSum(nums, target);
+    vector<int>result = s.twoSum(nums, target);
     if (!result.empty())
     {
         cout << "[" << result[0] << "," << result[1] << "]" << endl;
